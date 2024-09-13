1a7daf05f921c332e4725f61eb77319d0b4a4acd
Afekhide Gbadamosi <realafe21@gmail.com>
2024-09-13 08:25:41 +0100

    Setup landing page
    About to pull in auth

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,28 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "node_modules/preline/dist/*.js"
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    '50': '#fff8ed',
+                    '100': '#fff0d4',
+                    '200': '#ffdea8',
+                    '300': '#ffc570',
+                    '400': '#ffa137',
+                    '500': '#ff830c',
+                    '600': '#f06906',
+                    '700': '#c74e07',
+                    '800': '#9e3e0e',
+                    '900': '#7f350f',
+                    '950': '#451805',
+                },
+            }
+        },
     },
-    plugins: [],
+    plugins: [
+        require('preline/plugin')
+    ],
 }
