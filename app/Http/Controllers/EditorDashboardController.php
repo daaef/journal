5445212bef83aa10f9163335ab164d2b34266a3f
Nanichang Katzing <nanipaul68@gmail.com>
2024-10-09 14:04:03 +0100

    feat: route validation middleware

--- app/Http/Controllers/EditorDashboardController.php
+++ app/Http/Controllers/EditorDashboardController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class EditorDashboardController extends Controller
 {
     public function index(){
+        // dd(auth()->user()->hasRole('Editor'));
         return view('dashboard.editor.dashboard');
     }
 }
---
+++ app/Http/Middleware/EditorMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EditorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (!auth()->user()->hasRole('Editor')) {
+            $notification = array(
+                'message' => 'You are not authorized to access this page',
+                'alert-type' => 'error'
+            );
+            return redirect()->back()->with($notification);
+
+        }
+        return $next($request);
+    }
+}
