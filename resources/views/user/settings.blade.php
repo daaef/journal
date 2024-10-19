33808e16563df226fda7f45301b0e752dd15dc1e
Afekhide Gbadamosi <realafe21@gmail.com>
2024-10-19 19:41:41 +0100

    pulling

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -40,6 +40,20 @@
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
+                    <div>
+                        <label for="institution" class="block text-sm font-medium leading-6 text-gray-900">Institution</label>
+                        <div class="mt-2">
+                            <input id="institution" name="username" type="text" value="{{ $user->institution }}" autocomplete="institution"
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+                    <div>
+                        <label for="interests" class="block text-sm font-medium leading-6 text-gray-900">Interests</label>
+                        <div class="mt-2">
+                            <input id="interests" name="username" type="text" value="{{ $user->interests }}" autocomplete="interests"
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
                     <div class="w-full">
                         <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Country /
                             Region</label>
