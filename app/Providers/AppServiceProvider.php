1656c7a8342cb8b1997aad70f42d4101267cb35c
Afekhide Gbadamosi <realafe21@gmail.com>
2025-01-05 17:14:26 +0100

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Pulse\Facades\Pulse;
 
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
             'extra' => $user->email,
             'avatar' => $user->avatar_url,
         ]);
+        if (config('app.env') === 'production') { // Only in production
+            URL::forceScheme('https');
+        }
     }
 }
--- resources/views/components/layouts/editor_layout.blade.php
+++ resources/views/components/layouts/editor_layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title> {{ $title ?? 'JAPR Website | Dashboard' }}</title>
     <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
